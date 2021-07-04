c966f5d5e9e7451db77ad1434a592ec345daa80c
Ibrahim-Badwi <ib.a.badwi@gmail.com>
2021-07-04 13:59:18 +0300

    create hello endpoint

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get('/version', (req, res) => {
   res.send('1')
 })
 
+app.get('/hello/:name', (req, res) => {
+  res.send(`Hello ${req.params.name}.`)
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
